c86c4dec8341fa6b4b8c3e88e0e529a286c419ff
Sanath S Kamath <sanathkamath11@gmail.com>
2025-02-11 01:23:18 +0530

    Add files via upload

---
+++ aivirtualmouse/virtualmouse.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+import handtrackingmodule as htm
+import time
+from pynput.mouse import Controller, Button
+
+wCam, hCam = 640, 480
+frameR = 100  # Frame Reduction
+smoothening = 5
+
+pTime = 0
+plocX, plocY = 0, 0  # Previous location
+clocX, clocY = 0, 0  # Current location
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+detector = htm.handDetector(maxHands=1)
+mouse = Controller()
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]  # Index finger tip
+        x2, y2 = lmList[12][1:]  # Middle finger tip
+
+        fingers = detector.fingersUp()
+        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
+
+        if fingers[1] == 1 and fingers[2] == 0:
+            x3 = np.interp(x1, (frameR, wCam - frameR), (1920,0))
+            y3 = np.interp(y1, (frameR, hCam - frameR), (0, 1080))
+
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+
+            mouse.position = (clocX, clocY)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            plocX, plocY = clocX, clocY
+
+        if fingers[1] == 1 and fingers[2] == 1:
+            length, img, lineInfo = detector.findDistance(8, 12, img)
+            if length < 40:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                mouse.click(Button.left, 1)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
